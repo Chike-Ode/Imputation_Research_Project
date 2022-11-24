fe6c7042a5475ccf9750f1844e4b86d8f5b85392
Chike-Ode <chikeodenigbo@gmail.com>
2022-11-24 03:09:08 -0500

    improved viz, need to determine target column

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -8,34 +8,38 @@ plt.style.use("fivethirtyeight")
 
 # TODO image not outputting to folder and bar cut off
 
-def histogram(df,col,output_prefix,fig_dir,subtitle = '', show_graph = True, bins = 40):
+def histogram(df,col,output_prefix,fig_dir,subtitle = '', title = None, show_graph = True, bins = 40):
     fig, ax = plt.subplots()
     ax.hist(df[col], bins = bins,alpha=0.75)
-    title = col.replace('_',' ').title()
+    if title == None:
+        title = col.replace('_',' ').title()
     # ax.set_title(f'{title} ',fontsize=18.5, fontweight='semibold', color='#414141')
     plt.figtext(0.05, 1.05, title,fontsize=18.5, fontweight='semibold', color='#414141')
     plt.figtext(0.05, 1, subtitle,fontsize=16.5, color='#414141')
-    ax.set_xlabel(title)
+    ax.set_xlabel(col.replace('_',' ').title())
     ax.set_ylabel('Frequency')
     ax.axhline(y=0.1, color='#414141', linewidth=2.5)
     fig.tight_layout()
+    print(f'Image found in the "reports/figures" folder saved under {output_prefix}_{col}_hist.png')
+    plt.savefig(f'{fig_dir}{output_prefix}_{col}_hist.png')
     if show_graph == True:
         plt.show()
-    plt.savefig(f'{fig_dir}{output_prefix}_{col}_hist.png')
 
-def box_plot(df,col,subtitle, fig_dir, output_prefix, show_graph = True,):
-    title = col.replace('_',' ').title()
+def box_plot(df,col,subtitle, fig_dir, output_prefix, title = None, show_graph = True):
+    if title == None:
+        title = col.replace('_',' ').title()
     fig, ax = plt.subplots()
     plt.figtext(0.05, 1.05, title,fontsize=18.5, fontweight='semibold', color='#414141')
     plt.figtext(0.05, 1, subtitle,fontsize=16.5, color='#414141')
-    ax.set_xlabel(title)
+    ax.set_xlabel(col.replace('_',' ').title())
     ax.set_ylabel('Frequency')
     plt.boxplot(df[col],patch_artist=True)
     fig.tight_layout()
+    print(f'Image found in the "reports/figures" folder saved under {output_prefix}_{col}_boxplot.png')
+    plt.savefig(f"{fig_dir}{output_prefix}_{col}_boxplot.png")
     if show_graph == True:
         plt.show()
-    plt.savefig(f"{fig_dir}{output_prefix}_{col}_boxplot.png")
-
+    
 def bar_plot(df,x,y,title,subtitle,fig_dir,output_prefix,n_cols = 12,show_graph = True):
     fig, ax = plt.subplots()
     df = df.nlargest(n_cols,y)
@@ -43,9 +47,10 @@ def bar_plot(df,x,y,title,subtitle,fig_dir,output_prefix,n_cols = 12,show_graph 
     ax.barh(df[x],df[y],alpha=0.75)
     plt.figtext(0.05, 1.05, title,fontsize=18.5, fontweight='semibold', color='#414141')
     plt.figtext(0.05, 1, subtitle,fontsize=16.5, color='#414141')
-    ax.set_xlabel(x)
-    ax.set_ylabel(y)
+    ax.set_xlabel(y.replace('_',' ').title())
+    ax.set_ylabel(x.replace('_',' ').title())
     fig.tight_layout()
+    print(f'Image found in the "reports/figures" folder saved under {output_prefix}_{x}_{y}_barchart.png')
+    plt.savefig(f"{fig_dir}{output_prefix}_{x}_{y}_barchart.png")
     if show_graph == True:
         plt.show()
-    plt.savefig(f"{fig_dir}{output_prefix}_{x}_{y}_barchart.png")
