033390ab6309e0facf7146e256948f78a335e8c4
Chike-Ode <chikeodenigbo@gmail.com>
2022-11-25 00:05:28 -0500

    finished eda, select var to mask now to implement

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -8,7 +8,7 @@ plt.style.use("fivethirtyeight")
 
 # TODO image not outputting to folder and bar cut off
 
-def histogram(df,col,output_prefix,fig_dir,subtitle = '', title = None, show_graph = True, bins = 40):
+def histogram(df,col,output_prefix,fig_dir,subtitle = '', title = None, show_graph = True, bins = 40, percent_col=None,log_scale = None):
     fig, ax = plt.subplots()
     ax.hist(df[col], bins = bins,alpha=0.75)
     if title == None:
@@ -18,14 +18,29 @@ def histogram(df,col,output_prefix,fig_dir,subtitle = '', title = None, show_gra
     plt.figtext(0.05, 1, subtitle,fontsize=16.5, color='#414141')
     ax.set_xlabel(col.replace('_',' ').title())
     ax.set_ylabel('Frequency')
-    ax.axhline(y=0.1, color='#414141', linewidth=2.5)
+    if str(percent_col).lower() == 'x':
+        vals = ax.get_xticks()
+        ax.set_xticklabels(['{:,.0%}'.format(x) for x in vals])
+    if str(percent_col).lower() == 'y':
+        vals = ax.get_xticks()
+        ax.set_yticklabels(['{:,.0%}'.format(x) for x in vals])
+    if log_scale == True:
+        plt.gca().set_yscale("log")
+        ax.bar_label(ax.containers[0], label_type='edge')
+        plt.gca().axes.yaxis.set_ticklabels([])
+
+        ax.yaxis.set_ticks_position('none')
+        ax.set_ylabel('Log Frequency')
+        ax.axhline(y=0, color='#414141', linewidth=2.5)
+    else:
+        ax.axhline(y=0.1, color='#414141', linewidth=2.5)
     fig.tight_layout()
     print(f'Image found in the "reports/figures" folder saved under {output_prefix}_{col}_hist.png')
     plt.savefig(f'{fig_dir}{output_prefix}_{col}_hist.png',bbox_inches='tight')
-    if show_graph == True:
-        plt.show()
+    # if show_graph == True:
+    #     plt.show()
 
-def box_plot(df,col,subtitle, fig_dir, output_prefix, title = None, show_graph = True):
+def box_plot(df,col,subtitle, fig_dir, output_prefix, title = None, show_graph = True,percent_col = None):
     if title == None:
         title = col.replace('_',' ').title()
     fig, ax = plt.subplots()
@@ -34,13 +49,19 @@ def box_plot(df,col,subtitle, fig_dir, output_prefix, title = None, show_graph =
     ax.set_xlabel(col.replace('_',' ').title())
     ax.set_ylabel('Frequency')
     plt.boxplot(df[col],patch_artist=True)
+    if str(percent_col).lower() == 'x':
+        vals = ax.get_xticks()
+        ax.set_xticklabels(['{:,.0%}'.format(x) for x in vals])
+    if str(percent_col).lower() == 'y':
+        vals = ax.get_xticks()
+        ax.set_yticklabels(['{:,.0%}'.format(x) for x in vals])
     fig.tight_layout()
     print(f'Image found in the "reports/figures" folder saved under {output_prefix}_{col}_boxplot.png')
-    plt.savefig(f"{fig_dir}{output_prefix}_{col}_boxplot.png",bbox_inches='tight')
-    if show_graph == True:
-        plt.show()
+    plt.savefig(f"{fig_dir}{output_prefix}_{col}_boxplot.jpg",bbox_inches='tight')
+    # if show_graph == True:
+    #     plt.show()
     
-def bar_plot(df,x,y,title,subtitle,fig_dir,output_prefix,n_cols = 12,show_graph = True):
+def bar_plot(df,x,y,title,subtitle,fig_dir,output_prefix,n_cols = 12,show_graph = True, width = None, height = None,percent_col = None):
     fig, ax = plt.subplots()
     df = df.nlargest(n_cols,y)
     df = df.sort_values(y, ascending = True)
@@ -49,8 +70,17 @@ def bar_plot(df,x,y,title,subtitle,fig_dir,output_prefix,n_cols = 12,show_graph 
     plt.figtext(0.05, 1, subtitle,fontsize=16.5, color='#414141')
     ax.set_xlabel(y.replace('_',' ').title())
     ax.set_ylabel(x.replace('_',' ').title())
+    if width != None and height != None:
+        fig.set_size_inches(width, height)
+    if str(percent_col).lower() == 'x':
+        vals = ax.get_xticks()
+        ax.set_xticklabels(['{:,.0%}'.format(x) for x in vals])
+    if str(percent_col).lower() == 'y':
+        vals = ax.get_xticks()
+        ax.set_yticklabels(['{:,.0%}'.format(x) for x in vals])
     fig.tight_layout()
+    
     print(f'Image found in the "reports/figures" folder saved under {output_prefix}_{x}_{y}_barchart.png')
-    plt.savefig(f"{fig_dir}{output_prefix}_{x}_{y}_barchart.png",bbox_inches='tight')
-    if show_graph == True:
-        plt.show()
+    plt.savefig(f"{fig_dir}{output_prefix}_{x}_{y}_barchart.jpg",bbox_inches='tight')
+    # if show_graph == True:
+    #     plt.show()
